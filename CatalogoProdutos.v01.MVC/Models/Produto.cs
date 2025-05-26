14bd22d13da5a7ee54d0b91b51316e5aca060512
JandersonArantes <janderson_alves@hotmail.com>
2025-05-26 14:39:38 -0300

    Modelo.

---
+++ CatalogoProdutos.v01.MVC/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogoProdutos.v01.MVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O preço é obrigatório.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Informe um valor válido.")]
+        public double Preco { get; set; }
+
+    }
+}
