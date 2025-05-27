6327cf73b76fe56f71a98d7ce005e5188e078fe1
JandersonArantes <janderson_alves@hotmail.com>
2025-05-27 15:10:11 -0300

    Criação da Action Index

--- CatalogoProdutos.v01.MVC/Controllers/Produto.cs
+++ CatalogoProdutos.v01.MVC/Controllers/Produto.cs
@@ -1,23 +1,20 @@
 ﻿using CatalogoProdutos.v01.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CatalogoProdutos.v01.MVC.Controllers
-{
-    public class ProdutoController: Controller
-    {
+namespace CatalogoProdutos.v01.MVC.Controllers {
+    public class ProdutoController : Controller {
+        // GET: Produto/Create
         [HttpGet]
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             // Exibe o formulário para cadastrar o produto.
             return View();
         }
 
+        // POST: Produto/Create
         [HttpPost]
-        public ActionResult Create(Produto produto)
-        {
+        public ActionResult Create(Produto produto) {
             // Se os dados informados forem válidos, definir uma mensagem temporária de sucesso e 
-            if (ModelState.IsValid)
-            {
+            if (ModelState.IsValid) {
                 TempData["Mensagem"] = "Produto cadastrado com sucesso!";
                 return RedirectToAction("Create");
             }
@@ -25,6 +22,16 @@ namespace CatalogoProdutos.v01.MVC.Controllers
             return View(produto);
         }
 
+        // GET: Produto
+        public ActionResult Index() {
+            var produto = new List<Produto> {
+                new Produto{Id=1, Nome="Mouse", Categoria = "Periféricos"},
+                new Produto{Id=2, Nome="Teclado", Categoria = "Periféricos"},
+                new Produto{Id=3, Nome="Monitor", Categoria = "Vídeos"}
+            };
+
+            return View(produto);
+        }
 
     }
 }
